7a015b97b3b6669e3f1425d734d510c918d6da36
Nipun <1.madvoid@gmail.com>
2014-07-31 11:45:33 -0600

    Added Davis Individual

---
+++ Davis_Anemometer/Davis_Anemometer.ino
@@ -0,0 +1,62 @@
+/*
+  Analog Input
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13. 
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead().
+ 
+ The circuit:
+ * Potentiometer attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ * LED anode (long leg) attached to digital output 13
+ * LED cathode (short leg) attached to ground
+ 
+ * Note: because most Arduinos have a built-in LED attached 
+ to pin 13 on the board, the LED is optional.
+ 
+ 
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+ 
+ This example code is in the public domain.
+ 
+ http://arduino.cc/en/Tutorial/AnalogInput
+ 
+ */
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+unsigned long startTime;
+volatile unsigned long count = 0;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+}
+
+void loop() {
+
+  count = 0;
+  startTime = millis();
+  attachInterrupt(0, counter, RISING);
+  while(millis() - startTime < 5000){
+  }
+  detachInterrupt(0);
+
+
+  Serial.print(analogRead(sensorPin));
+  Serial.print(" || ");
+  Serial.println(count);
+  Serial.println("\nStarting...");
+}
+
+void counter(){
+  count++;
+}
+
